f0d8f3ca7fcbc012f8de5075e4588eac0862806a
NourShloul <shloulnour31@gmail.com>
2024-08-19 18:06:38 +0300

    updated

---
+++ Task1Core/Task1Core/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task1Core.Models;
+
+namespace Task1Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+        public CategoriesController(MyDbContext db)
+        {
+                _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            var category = _db.Categories.ToList();
+            return Ok(category);
+        }
+        [HttpGet("id")]
+        public IActionResult GetCategoryById(int id)
+        {
+            var category = _db.Categories.Where(c => c.CategoryId == id).FirstOrDefault();
+            return Ok(category);
+        }
+    }
+}
---
+++ Task1Core/Task1Core/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task1Core.Models;
+
+namespace Task1Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+        public ProductsController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var product = _db.Products.Select(b => new
+            {
+                b.ProductId,
+                b.ProductName,
+                b.Description,
+                b.Price,
+                b.CategoryId,
+                b.ProductImage,
+                b.Category
+            });
+            return Ok(product);
+        }
+        [HttpGet("id")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _db.Products.Where(c => c.ProductId == id).FirstOrDefault();
+            return Ok(product);
+        }
+    }
+}
